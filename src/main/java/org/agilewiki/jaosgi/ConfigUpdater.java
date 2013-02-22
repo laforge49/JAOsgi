a0509c4591610dd14c348a33b840bf13e6c10b88
Bill La Forge <laforge49@gmail.com>
2013-02-22 23:52:27 +0530

    reverted last change

--- src/main/java/org/agilewiki/jaosgi/ConfigUpdater.java
+++ src/main/java/org/agilewiki/jaosgi/ConfigUpdater.java
@@ -1,28 +1,7 @@
-/*
- * Copyright 2013 Bill La Forge
- *
- * This file is part of AgileWiki and is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License (LGPL) as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
- *
- * This code is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
- * or navigate to the following url http://www.gnu.org/licenses/lgpl-2.1.txt
- *
- * Note however that only Scala, Java and JavaScript files are being covered by LGPL.
- * All other files are covered by the Common Public License (CPL).
- * A copy of this license is also included and can be
- * found as well at http://www.opensource.org/licenses/cpl1.0.txt
- */
 package org.agilewiki.jaosgi;
 
+import org.agilewiki.jactor.JAMailboxFactory;
+import org.agilewiki.jactor.MailboxFactory;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.ServiceRegistration;
 import org.osgi.service.cm.ConfigurationException;
@@ -31,6 +10,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.Dictionary;
+import java.util.Hashtable;
 import java.util.List;
 
 public final class ConfigUpdater implements ManagedService {
@@ -57,7 +37,11 @@ public final class ConfigUpdater implements ManagedService {
             throw new ConfigurationException("threadCount", "not an int: "+tc, ex);
         }
         logger.info("threadCount: " + threadCount);
-        new OMailboxFactory(threadCount);
+        MailboxFactory mailboxFactory = JAMailboxFactory.newMailboxFactory(threadCount);
+        registrations.add(context.registerService(
+                MailboxFactory.class.getName(),
+                mailboxFactory,
+                new Hashtable<String, Object>()));
 
         //todo
     }
