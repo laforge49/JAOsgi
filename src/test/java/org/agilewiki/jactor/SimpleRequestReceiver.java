17a68ef37dbbfa12108b409d6c17e2622866c8eb
Bill La Forge <laforge49@gmail.com>
2013-03-03 17:51:50 +0530

    JActor put back in its own project

--- src/test/java/org/agilewiki/jactor/SimpleRequestReceiver.java
+++
@@ -1,8 +0,0 @@
-package org.agilewiki.jactor;
-
-import org.agilewiki.jactor.lpc.TargetActor;
-
-public interface SimpleRequestReceiver extends TargetActor {
-    public void processRequest(SimpleRequest request, RP rp)
-            throws Exception;
-}
--- src/test/java/org/agilewiki/jactor/lpc/syncTiming/Echo.java
+++
@@ -1,8 +0,0 @@
-package org.agilewiki.jactor.lpc.syncTiming;
-
-import org.agilewiki.jactor.lpc.JLPCActor;
-
-public class Echo extends JLPCActor {
-    public void echo() {
-    }
-}
