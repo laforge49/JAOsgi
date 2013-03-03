ccae66553b32dc759f00e4872a95c7a1ab062bd0
Bill La Forge <laforge49@gmail.com>
2013-03-03 09:12:40 +0530

    new LongStringBMapJidTest

---
+++ src/test/java/org/agilewiki/jid/collection/vlenc/map/LongStringBMapJidTest.java
@@ -0,0 +1,48 @@
+package org.agilewiki.jid.collection.vlenc.map;
+
+import junit.framework.TestCase;
+import org.agilewiki.jaosgi.JABundleContext;
+import org.agilewiki.jaosgi.JAFactoryLocator;
+import org.agilewiki.jaosgi.JidFactories;
+import org.agilewiki.jid.scalar.vlens.string.StringJid;
+
+public class LongStringBMapJidTest extends TestCase {
+    public void test() throws Exception {
+        JAFactoryLocator factoryLocator = JidFactories.createNoOsgiFactoryLocator(1);
+        JABundleContext jaBundleContext = JABundleContext.getJABundleContext(factoryLocator);
+        try {
+            BMapJid<Long, StringJid> m = (BMapJid) factoryLocator.
+                    newActor(JidFactories.LONG_STRING_BMAP_JID_TYPE);
+            assertEquals(0, m.size());
+            assertTrue(m.kMake(1L));
+            assertFalse(m.kMake(1L));
+            assertEquals(1, m.size());
+            MapEntry<Long, StringJid> me = m.iGet(0);
+            assertEquals((Long) 1L, me.getKey());
+            StringJid v = m.kGet(1L);
+            assertEquals(v, me.getValue());
+            assertEquals(me, m.getCeiling(0L));
+            assertEquals(me, m.getCeiling(1L));
+            assertNull(m.getCeiling(2L));
+            assertEquals(me, m.getHigher(0L));
+            assertNull(m.getHigher(1L));
+            m.empty();
+            assertEquals(0, m.size());
+            assertTrue(m.kMake(1L));
+            assertEquals(1, m.size());
+            me = m.iGet(0);
+            assertEquals((Long) 1L, me.getKey());
+            m.iRemove(0);
+            assertEquals(0, m.size());
+            assertTrue(m.kMake(1L));
+            assertEquals(1, m.size());
+            me = m.iGet(0);
+            assertEquals((Long) 1L, me.getKey());
+            assertFalse(m.kRemove(0L));
+            assertTrue(m.kRemove(1L));
+            assertEquals(0, m.size());
+        } finally {
+            jaBundleContext.stop(0);
+        }
+    }
+}
