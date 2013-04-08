525f5d450248937e29e8e6b9af646b9d7744be9e
Bill La Forge <laforge49@gmail.com>
2013-04-08 17:04:43 +0530

    updated PALong

--- src/main/java/org/agilewiki/incdes/PALong.java
+++ src/main/java/org/agilewiki/incdes/PALong.java
@@ -5,5 +5,9 @@ import org.agilewiki.pactor.Request;
 public interface PALong extends IncDes {
     Request<Long> getLongReq();
 
+    Long getValue();
+
     Request<Void> setLongReq(final Long _v);
+
+    void setValue(final Long _v) throws Exception;
 }
