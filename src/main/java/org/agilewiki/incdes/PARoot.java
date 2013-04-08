f7e9b83ab1f67e273faa4a426b3efd557ce96c3f
Bill La Forge <laforge49@gmail.com>
2013-04-08 17:11:21 +0530

    updated PARoot

--- src/main/java/org/agilewiki/incdes/PARoot.java
+++ src/main/java/org/agilewiki/incdes/PARoot.java
@@ -1,4 +1,10 @@
 package org.agilewiki.incdes;
 
 public interface PARoot extends PABox {
+    int save(final byte[] _bytes, final int _offset) throws Exception;
+
+    int load(final byte[] _bytes, final int _offset, final int _length) throws Exception;
+
+    void load(final byte[] _bytes)
+            throws Exception;
 }
