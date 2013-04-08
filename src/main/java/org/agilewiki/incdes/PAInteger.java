89be5f414cb735f24bd2075b480ed6658e1bae7e
Bill La Forge <laforge49@gmail.com>
2013-04-08 17:02:18 +0530

    updated PAInteger

--- src/main/java/org/agilewiki/incdes/PAInteger.java
+++ src/main/java/org/agilewiki/incdes/PAInteger.java
@@ -5,5 +5,9 @@ import org.agilewiki.pactor.Request;
 public interface PAInteger extends IncDes {
     Request<Integer> getIntegerReq();
 
+    Integer getValue();
+
     Request<Void> setIntegerReq(final Integer _v);
+
+    void setValue(final Integer _v) throws Exception;
 }
