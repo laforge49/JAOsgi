0ef3e8a4ad0e15671eee0fdc77a3abf1f00119d0
Bill La Forge <laforge49@gmail.com>
2013-04-08 17:00:15 +0530

    Updated PAFloat

--- src/main/java/org/agilewiki/incdes/PAFloat.java
+++ src/main/java/org/agilewiki/incdes/PAFloat.java
@@ -5,5 +5,9 @@ import org.agilewiki.pactor.Request;
 public interface PAFloat extends IncDes {
     Request<Float> getFloatReq();
 
+    Float getValue();
+
     Request<Void> setFloatReq(final Float _v);
+
+    void setValue(final Float _v) throws Exception;
 }
