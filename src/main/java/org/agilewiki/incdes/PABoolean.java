8d7879ba9e2deead00d4b16dcd35499f7b286022
Bill La Forge <laforge49@gmail.com>
2013-04-08 15:58:49 +0530

    updated PABoolean

--- src/main/java/org/agilewiki/incdes/PABoolean.java
+++ src/main/java/org/agilewiki/incdes/PABoolean.java
@@ -5,5 +5,9 @@ import org.agilewiki.pactor.Request;
 public interface PABoolean extends PAIncDes {
     Request<Boolean> getBooleanReq();
 
+    Boolean getValue();
+
     Request<Void> setBooleanReq(final Boolean _v);
+
+    void setValue(final Boolean v) throws Exception;
 }
