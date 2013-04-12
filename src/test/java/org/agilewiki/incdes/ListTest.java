bd6f34048ac40afe0d87eb81519f869ffdfd42e0
Bill La Forge <laforge49@gmail.com>
2013-04-12 11:14:26 +0530

    ListTest now covers both simple and balanced lists

--- src/test/java/org/agilewiki/incdes/ListTest.java
+++ src/test/java/org/agilewiki/incdes/ListTest.java
@@ -3,23 +3,30 @@ package org.agilewiki.incdes;
 import junit.framework.TestCase;
 
 public class ListTest extends TestCase {
-    public void test() throws Exception {
+    public void test1() throws Exception {
+        t(IncDesFactories.PASTRING_LIST, 8, 12, 20, 24, 16);
+    }
 
+    public void test2() throws Exception {
+        t(IncDesFactories.PASTRING_BLIST, 20, 24, 32, 36, 28);
+    }
+
+    void t(String lt, int elsl, int nslsl, int sls, int l2sl2, int l2sl3) throws Exception {
         FactoryLocator factoryLocator = IncDesFactories.createFactoryLocator();
         Context jaBundleContext = Context.get(factoryLocator);
         try {
-            PAList<PAString> l0 = (PAList) factoryLocator.newJid(IncDesFactories.PASTRING_LIST);
+            PAList<PAString> l0 = (PAList) factoryLocator.newJid(lt);
             int l0sl = l0.getSerializedLength();
-            assertEquals(8, l0sl);
+            assertEquals(elsl, l0sl);
             PAList<PAString> l1 = (PAList) l0.copyReq(null).call();
             int l1sl = l1.getSerializedLength();
-            assertEquals(8, l1sl);
+            assertEquals(elsl, l1sl);
             l1.iAddReq(0).call();
             l1sl = l1.getSerializedLength();
-            assertEquals(12, l1sl);
+            assertEquals(nslsl, l1sl);
             PAList<PAString> l2 = (PAList) l1.copyReq(null).call();
             int l2sl = l2.getSerializedLength();
-            assertEquals(12, l2sl);
+            assertEquals(nslsl, l2sl);
             PAString s0 = IncDesFactories.createPAString(factoryLocator, null, null);
             s0.setStringReq("Hi").call();
             int s0sl = s0.getSerializedLength();
@@ -28,16 +35,16 @@ public class ListTest extends TestCase {
             assertEquals(8, s0bs.length);
             l2.iAddReq(-1, s0bs).call();
             l2sl = l2.getSerializedLength();
-            assertEquals(20, l2sl);
+            assertEquals(sls, l2sl);
             l2.iSetReq(0, s0bs).call();
             l2sl = l2.getSerializedLength();
-            assertEquals(24, l2sl);
+            assertEquals(l2sl2, l2sl);
             l2.iRemoveReq(0).call();
             l2sl = l2.getSerializedLength();
-            assertEquals(16, l2sl);
+            assertEquals(l2sl3, l2sl);
             l2.emptyReq().call();
             l2sl = l2.getSerializedLength();
-            assertEquals(8, l2sl);
+            assertEquals(elsl, l2sl);
         } finally {
             jaBundleContext.stop(0);
         }
