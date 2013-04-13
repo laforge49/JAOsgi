0dd9b5eb0612e704984e98de11c362159d91c0a0
Bill La Forge <laforge49@gmail.com>
2013-04-13 09:49:15 +0530

    LongPAStringBMapTest is working

--- src/test/java/org/agilewiki/incdes/LongPAStringBMapTest.java
+++ src/test/java/org/agilewiki/incdes/LongPAStringBMapTest.java
@@ -1,27 +1,22 @@
-package org.agilewiki.jid.collection.vlenc.map;
+package org.agilewiki.incdes;
 
 import junit.framework.TestCase;
-import org.agilewiki.incdes.IncDesFactories;
-import org.agilewiki.incdes.Context;
 import org.agilewiki.incdes.impl.collection.vlenc.map.BMap;
-import org.agilewiki.incdes.impl.collection.vlenc.map.MapEntryImpl;
-import org.agilewiki.incdes.impl.scalar.vlens.PAStringImpl;
-import org.agilewiki.incdes.impl.factory.FactoryLocatorImpl;
 
-public class LongStringBMapJidTest extends TestCase {
+public class LongPAStringBMapTest extends TestCase {
     public void test() throws Exception {
-        FactoryLocatorImpl factoryLocator = IncDesFactories.createFactoryLocator(1);
+        FactoryLocator factoryLocator = IncDesFactories.createFactoryLocator();
         Context jaBundleContext = Context.get(factoryLocator);
         try {
-            BMap<Long, PAStringImpl> m = (BMap) factoryLocator.
+            PAMap<Long, PAString> m = (PAMap) factoryLocator.
                     newJid(IncDesFactories.LONG_PASTRING_BMAP);
             assertEquals(0, m.size());
             assertTrue(m.kMake(1L));
             assertFalse(m.kMake(1L));
             assertEquals(1, m.size());
-            MapEntryImpl<Long, PAStringImpl> me = m.iGet(0);
+            MapEntry<Long, PAString> me = m.iGet(0);
             assertEquals((Long) 1L, me.getKey());
-            PAStringImpl v = m.kGet(1L);
+            PAString v = m.kGet(1L);
             assertEquals(v, me.getValue());
             assertEquals(me, m.getCeiling(0L));
             assertEquals(me, m.getCeiling(1L));
